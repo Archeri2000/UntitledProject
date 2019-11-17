65235e4a6940520b7fe4b34a3527f4206dfde3fe
kirath2205 <44152744+kirath2205@users.noreply.github.com>
2019-11-17 20:32:16 +0800

    Update CineplexRepository.java

--- src/main/com/repositories/CineplexRepository.java
+++ src/main/com/repositories/CineplexRepository.java
@@ -10,17 +10,42 @@ import java.util.*;
 import static main.com.utils.SerialisationUtils.*;
 import static main.com.utils.SerialisationUtils.deserialiseList;
 
+/** This class acts as a repository for storing Cineplexes
+ * @author SS1 Group 6
+ * @version 13
+ */
 public class CineplexRepository implements ISerialisable {
+    
+    /**Creation of a Cineplex hashmap
+	 * 
+	 */
     private HashMap<String, Cineplex> cineplexHashMap = new HashMap<>();
 
+    /**
+     * Get the cineplex details from the cineplexHashMap
+     * @param name of the cineplex
+     * @return cineplex name
+     */
+    
     public Cineplex GetCineplex(String name){
         if(cineplexHashMap.containsKey(name)) return cineplexHashMap.get(name);
         return null;
     }
+    
+    /**
+     * Get a list of cineplexes
+     * @return list of cineplexes
+     */
 
     public List<Cineplex> getCineplexes(){
         return new ArrayList<>(cineplexHashMap.values());
     }
+    
+    /**
+     * Create a cineplex
+     * @param name of the cineplex
+     * @return cineplex object
+     */
 
     public Cineplex createCineplex(String name){
         if(!cineplexHashMap.containsKey(name)){
@@ -30,6 +55,11 @@ public class CineplexRepository implements ISerialisable {
         }
         return null;
     }
+    
+    /**
+     * Serialize cineplex list
+     * @return serialised string
+     */
     //TODO
     @Override
     public String toSerialisedString() {
@@ -39,6 +69,10 @@ public class CineplexRepository implements ISerialisable {
         );
     }
 
+    
+    /** Deserialize cineplex list
+     * 
+     */
     @Override
     public ISerialisable fromSerialisedString(String s) throws InvalidPropertiesFormatException {
         _static_manager = this;
@@ -51,19 +85,34 @@ public class CineplexRepository implements ISerialisable {
         return getInstance();
     }
 
+    /**
+     * Create a static CineplexRepository object
+     */
     private static CineplexRepository _static_manager = new CineplexRepository();
 
+    
+    /**
+     * Get the instance of CineplexRepository
+     * @return CineplexRepository object
+     */
+    
     public static CineplexRepository getInstance(){
         if(_static_manager == null){
             _static_manager = new CineplexRepository();
         }
         return _static_manager;
     }
-
+    /**
+     * CineplexRepository constructor
+     */
     public CineplexRepository(){
         _static_manager = this;
     }
-
+    /**
+      * Remove a cineplex from the cineplex hashmap
+      * @param name of the cineplex
+      * @return boolean value
+      */
     public boolean removeCineplex(String name) {
         if(cineplexHashMap.containsKey(name)){
             cineplexHashMap.get(name).RemoveCineplex();
