bdccebce83e35d83ebf8bb38c672e0c97647074d
anranpoopoo <56906467+anranpoopoo@users.noreply.github.com>
2019-11-17 20:17:17 +0800

    Add files via upload

--- src/main/com/view/HistoryView.java
+++ src/main/com/view/HistoryView.java
@@ -6,10 +6,22 @@ import main.com.services.HistoryQueryService;
 import java.util.List;
 import java.util.Scanner;
 
+/** 
+ * Customer use this class view past bookings
+ * @author SS1 Group 6
+ * @version 13
+ */
 public class HistoryView {
 
+	/** 
+     * Create a history query service object
+     */
     HistoryQueryService HQS = new HistoryQueryService();
 
+    /** 
+     * Method to retrieve past bookings of a customer
+     * @return list of bookings made in the past
+     */
     public List<Booking> getPastBooking() {
         int userMobile;
         System.out.println(" Enter User mobile number");
@@ -17,6 +29,6 @@ public class HistoryView {
         Scanner sc = new Scanner(System.in);
         userMobile = sc.nextInt();
         sc.nextLine();
-		return HQS.getHistoryBookings(userMobile); // need code to retrieve booking history
+		return HQS.getHistoryBookings(userMobile);  // need code to retrieve booking history
     }
 }
