23fe5c5faa5b8500da9ba37c2ef255c2bea6669c
anranpoopoo <56906467+anranpoopoo@users.noreply.github.com>
2019-11-17 17:55:47 +0800

    Add files via upload

--- src/main/com/services/CineplexQueryService.java
+++ src/main/com/services/CineplexQueryService.java
@@ -7,23 +7,59 @@ import main.com.repositories.CineplexRepository;
 
 import java.util.List;
 
+/** 
+ * This class helps user to search for cineplexes and cinemas
+ * @author SS1 Group 6
+ * @version 13
+ */
 public class CineplexQueryService{
+	
+	/** 
+     * Create a cineplex repository
+     */
     private CineplexRepository _repo = CineplexRepository.getInstance();
 
+    /** 
+     * Get the name of cineplex
+     * @param name
+     * @return this cineplex's name
+     */
     public Cineplex GetCineplex(String name){
         return _repo.GetCineplex(name);
     }
-
+    
+    /** 
+     * Get the list of cineplexes
+     * @return list of cineplexes
+     */
     public List<Cineplex> getCineplexes(){
         return _repo.getCineplexes();
     }
+    
+    /** 
+     * Get the name of cinema
+     * @param name 		name of cineplex
+     * @param cinema ID
+     * @return this cinema's name
+     */
     public Cinema GetCinema(Cineplex cineplex, String CinemaID){
         return cineplex.getCinema(CinemaID);
     }
+    
+    /** 
+     * Get the list of cinemas' name 
+     * @param name 		name of cineplex
+     * @return list of cinemas' name under this cineplex
+     */
     public List<Cinema> GetCinemas(Cineplex Cineplex){
         return Cineplex.getCinemas();
     }
 
+    /** 
+     * Get the list of movie shows
+     * @param name 		name of cinema
+     * @return list of movies showing at this cinema
+     */
     public List<MovieShowing> GetShows(Cinema cinema){
         return cinema.getShows();
     }
