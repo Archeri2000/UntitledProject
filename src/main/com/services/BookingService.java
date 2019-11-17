f8a86df322a17b4ea9e05f018703ffbb4e2dc888
anranpoopoo <56906467+anranpoopoo@users.noreply.github.com>
2019-11-17 17:01:46 +0800

    Add files via upload

--- src/main/com/services/BookingService.java
+++ src/main/com/services/BookingService.java
@@ -4,24 +4,59 @@ import main.com.entities.*;
 
 import main.com.repositories.CustomerRepository;
 
-
 import java.util.ArrayList;
 
+/** 
+ * This class helps a customer to make a booking 
+ */
+
 public class BookingService{
+	
+	 /** 
+     * Create a customer repository
+     */
 
     private CustomerRepository _repo = CustomerRepository.getInstance();
 
+    
+    /** 
+     * Get the customer object
+     * @param phone 	mobile number of customer
+     * @return customer object 
+     */
      public Customer getCustomerByPhone(int phone){
          return _repo.getCustomer(phone);
      }
+     
+     
+     /** 
+      * Create and get the customer object
+      * @param name		name of customer
+      * @param email	email of customer
+      * @param phone 	mobile number of customer
+      * @return customer object 
+      */
      public Customer createCustomer(String name, String email, int phone){
          return _repo.createCustomer(name, email, phone);
      }
+     
+     
+     /** 
+      * Get seating plan for the movie showing
+      * @param showing	the movie showing
+      * @return the seating plan of the movie showing
+      */
      public Seating getSeating(MovieShowing showing) {
          return showing.getSeatingplan();
      }
-
-     public boolean selectSeats(String seat, MovieShowing showing, int age){
+     
+     /** 
+      * To determine if seats are occupied and to select seats
+      * @param seat			the seat the customer wish to select
+      * @param showing		the movie showing
+      * @return True/False	if the seat is successfully selected
+      */
+     public boolean selectSeats(String seat, MovieShowing showing){
          Seating seatplan = showing.getSeatingplan();
 
          ArrayList<String> selectedSeats = new ArrayList<>();
